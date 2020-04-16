437932ea28abe8f9e9e6c47ddc212d18a752f736
bernibra <bernat.bramon@gmail.com>
2020-04-16 14:56:49 +0200

    Uncommited changes, not sure where I'm at

--- code/process-data.R
+++ code/process-data.R
@@ -5,8 +5,9 @@ library(stringr)
 ## Local library
 source("useful-tools.R")
 
-## Global
-rechalp_directory <- "/Volumes/GeoData_RECHALP/Climate/CHclim25/"
+## Global variables
+# rechalp_directory <- "/Volumes/GeoData_RECHALP/Climate/CHclim25/"
+rechalp_directory <- "../../competition-networks/data/raw/climatic-data/important-yearly-variables/bio1_tmean_8110.tif"
 wsl_directory <- "../data/raw/climate-data/"
 raw_sites <- "../data/raw/sites/sites.csv"
 processed_folder <- "../data/processed/"
@@ -19,10 +20,22 @@ sites$Codes <- sapply(1:nrow(sites), function(x) paste("site", x, sep=''))
 # Write sites' codes
 write.csv(file = paste(processed_folder, "sites.csv",sep=""), x = data.frame(code=sites$Codes, site=str_trim(sites$Site), canton=sites$Canton))
 
-# Find climatic variables for each site
-for(idx in 1:nrow(sites)){
-
-  # Define name of the output file
-  filename <- paste(processed_folder, "sites", sites$Codes[idx], ".csv", sep = "")
+# Check if you have access to the UNIL data
+if(!dir.exists(rechalp_directory)){
   
+  stop("You are not connected to the UNIL repository where the Rechalp data is located.")
+
+}else{
+  
+  # Find climatic variables for each site
+  for(idx in 1:nrow(sites)){
+    
+    # Define name of the output file
+    filename <- paste(processed_folder, sites$Codes[idx], ".csv", sep = "")
+    
+    # Figure out whether or not the data is from the Rechalp region
+    reshalp <- is.reshalp(folder = rechalp_directory, lon = sites$Long_WGS84[idx], lat = sites$Lat_WGS84[idx])
+    
+  }
+
 }
--- code/useful-tools.R
+++ code/useful-tools.R
@@ -1,10 +1,36 @@
 
-
+#Download WSL data if not there
 download_climatic <- function(res=0.5, meanlat, meanlong){
+  
   bioclim.data <- getData(name = "worldclim",
                           var = "bio",
                           res = res,
                           lat=meanlat,
                           lon=meanlong,
                           path = "../../data/raw/climatic-data/")
+
 }
+
+#Convert Lat and Lon to N and E
+transform_coordinates <- function(lon, lat){
+
+  cord.dec = SpatialPoints(cbind(lon, lat), proj4string=CRS("+proj=longlat"))
+
+  swissgrid <- "+proj=somerc +init=world:CH1903"
+  #Alternatively, you can use: swissgrid <- "+init=epsg:21781"
+
+  cord.UTM <- spTransform(cord.dec, swissgrid)
+  return(list(N=cord.UTM@coords[2], E=cord.UTM@coords[1]))
+
+}
+
+#Check if the site is in the Rechalp area
+is.reshalp <- function(folder, lon, lat){
+
+  # file <- paste(folder, "biovars/yearly/bio1_tmean1981_Rechalp_ngb5_mwconic.tif")
+  file <- folder
+  bioclim.data <- stack("../../competition-networks/data/raw/climatic-data/important-yearly-variables/bio12_p_8110.tif")
+  crs(bioclim.data)<-"+proj=somerc +init=world:CH1903"
+  
+}
+
